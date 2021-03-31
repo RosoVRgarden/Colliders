baa1773407af0567844fed2b428c4b7a33ccccc3
nnimar <64356425+nnimar@users.noreply.github.com>
2021-03-31 13:42:19 +0800

    init

---
+++ Assets/sound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour
+{
+
+    AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collision");
+        Debug.Log(gameObject.name + " has collided with " + collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("sound"))
+        {
+            print("play");
+            source.Play();
+        }
+    }
+
+}
---
+++ Assets/telepo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class telepo : MonoBehaviour
+{
+    public Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(2.9f, 0.41f, 0f);
+        targetPos = new Vector3(0.49f, 0.11f, 4.42f);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collision");
+
+        if (collision.gameObject.CompareTag("sound"))
+        {
+            print("collided");
+            transform.position = targetPos;
+
+        }
+    }
+
+
+}
